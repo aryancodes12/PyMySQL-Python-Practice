39aa8b365ecb3b24960537f6fb7e921d22232186
Aryan Gupta <aryansynthh@gmail.com>
2026-06-22 21:04:08 +0530

    Created update grade fucntion

--- update.py
+++ update.py
@@ -1,14 +1,25 @@
+import pymysql
 from db import get_db
 
-conn = get_db()
-cursor = conn.cursor()
 
-cursor.execute(
-    "UPDATE std SET age = %s WHERE id = %s",
-    (20, 5)
-)
+def update_grade():
+    conn = cursor = None
 
-if cursor.rowcount == 0:
-    print("No student with that ID - nothing was changed")
-else:
-    print(f"{cursor.rowcount} student(s) updated")
+    conn = get_db()
+    cursor = conn.cursor()
+
+    grade = input("Enter new grade: ").strip().upper()
+    id = int(input("Enter id: "))
+
+    cursor.execute(
+        "UPDATE std SET grade = %s WHERE id = %s",
+        (grade, id)
+        )
+    
+    if cursor.rowcount == 0:
+        print("No student with that ID - nothing was changed")
+    else:
+        print(f"{cursor.rowcount} student(s) updated")
+
+
+update_grade()
